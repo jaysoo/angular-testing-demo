62b888a2608316b0a0d0705e32c325c8bea97c34
Jack Hsu <jack.hsu@gmail.com>
2025-03-04 22:03:47 -0500

    switch to vitest and add lib

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { UiComponent } from '@acme/ui';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, UiComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
